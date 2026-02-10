a60188c909f473001b797a1db54463aa10d6009c
tunafish2k <lyt080529@163.com>
2026-02-11 04:57:07 +0800

    feat: 初步实现了后端接口

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,5 +1,21 @@
+import { simpleGetPhoto } from './remote';
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		const pathname = url.pathname.slice(1);
+		const [photoId] = pathname.split('/');
+		if (Number.isNaN(parseInt(photoId)))
+			return Response.json(
+				{
+					message: 'pathname should be /${id}, where id is an integer',
+				},
+				{
+					status: 400,
+					statusText: 'Bad Request',
+				},
+			);
+		const photo = await simpleGetPhoto(photoId);
+		return Response.json(photo);
 	},
 } satisfies ExportedHandler<Env>;
