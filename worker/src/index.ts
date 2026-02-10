9da10bd52ccefbcdbe16b28ac90c2579a2bb43c2
tunafish2k <lyt080529@163.com>
2026-02-11 01:27:59 +0800

    feat: 实现了请求禁漫时的headers参数中的token部分

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,3 +1,20 @@
+import { createHash } from 'node:crypto';
+
+const SECRET = '18comicAPP';
+const SECRET_CONTENT = '18comicAPPContent';
+
+function getTimestampSeconds() {
+	return Math.floor(Date.now() / 1000);
+}
+
+function getToken(timestampSeconds: number, secret: string) {
+	return createHash('md5').update(timestampSeconds.toString()).update(secret).digest().toString('hex');
+}
+
+function getTokenParam(timestampSeconds: number, version: string) {
+	return `${timestampSeconds.toString()},${version}`;
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		return new Response('Hello World!');
